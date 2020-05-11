184b61a2b7fb15e2a334b2ad9a931962eda31cca
vien.fu <vien.fu@ucloud.cn>
2020-05-11 19:04:51 +0800

    修复nginx配置error，优化字符串首尾自动去空格

--- snapmgr/config.py
+++ snapmgr/config.py
@@ -4,11 +4,11 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
 
 environs = os.environ
-mysql_host = environs['mysql_host'] if environs.get('mysql_host') else '127.0.0.1'
-mysql_name = environs['mysql_name'] if environs.get('mysql_name') else 'cmpvirtmgr'
-mysql_user = environs['mysql_user'] if environs.get('mysql_user') else 'cmpvirtmgr'
-mysql_password = environs['mysql_password'] if environs.get('mysql_password') else 'cmpvirtmgr123'
-mysql_port = environs['mysql_port'] if environs.get('mysql_port') else '3306'
+mysql_host = environs['mysql_host'].strip() if environs.get('mysql_host') else '127.0.0.1'
+mysql_name = environs['mysql_name'].strip() if environs.get('mysql_name') else 'cmpvirtmgr'
+mysql_user = environs['mysql_user'].strip() if environs.get('mysql_user') else 'cmpvirtmgr'
+mysql_password = environs['mysql_password'].strip() if environs.get('mysql_password') else 'cmpvirtmgr123'
+mysql_port = environs['mysql_port'].strip() if environs.get('mysql_port') else '3306'
 
 
 def _mysql_config(host, name, user, password, port):
@@ -40,12 +40,12 @@ class DevelopmentConfig(BasicConfig):
     SCHEDULER_API_PREFIX = '/snapmgr_api'
 
     # cinder client configuration
-    AUTH_URL = environs['auth_url'] if environs.get('auth_url') else "http://172.31.11.204:5000/v2.0"
-    AUTH_VERSION = environs['auth_version'] if environs.get('auth_version') else "2"
-    USERNAME = environs['username'] if environs.get('username') else "admin"
-    PASSWORD = environs['password'] if environs.get('password') else "admin"
-    TENANT_NAME = environs['tenant_name'] if environs.get('tenant_name') else "admin"
-    REGION_NAME = environs['region_name'] if environs.get('region_name') else "RegionOne"
+    AUTH_URL = environs['auth_url'].strip() if environs.get('auth_url') else "http://172.31.11.204:5000/v2.0"
+    AUTH_VERSION = environs['auth_version'].strip() if environs.get('auth_version') else "3"
+    USERNAME = environs['username'].strip() if environs.get('username') else "admin"
+    PASSWORD = environs['password'].strip() if environs.get('password') else "admin"
+    TENANT_NAME = environs['tenant_name'].strip() if environs.get('tenant_name') else "admin"
+    REGION_NAME = environs['region_name'].strip() if environs.get('region_name') else "RegionOne"
 
     # 定时任务持久化设置
     SCHEDULER_JOBSTORES = {
